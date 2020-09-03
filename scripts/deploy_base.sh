505f26897e06420c07a9a5e5bfd17224a4c72fc2
“bottkars” <Karsten.bott@bottnet.de>
2020-09-03 07:22:43 +0200

    gitignotr

--- scripts/deploy_base.sh
+++ scripts/deploy_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp *.sh /root
-echo "forking to configure-nve-azs.sh, monitor install.log for root or AVIGUI"
-su root -c "nohup /root/configure-nve-azs.sh >/dev/null 2>&1 &"
+echo "forking to configure-dde-azs.sh, monitor install.log for root or AVIGUI"
+su root -c "nohup /root/configure-ddve-azs.sh >/dev/null 2>&1 &"
